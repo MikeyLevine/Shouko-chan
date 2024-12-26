5ef674a9a8cb7acfa053dacd81f4d24404c339f6
Mikey Levine <mikeylevineph@gmail.com>
2024-12-26 03:01:08 +0100

    Your commit message

--- cogs/general/help.py
+++ cogs/general/help.py
@@ -1,26 +1,27 @@
 import discord
 from discord.ext import commands
+from discord import app_commands
 
 class Help(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="help", description="Show help for all commands")
-    async def help(self, ctx):
+    @app_commands.command(name="help", description="Show help for all commands")
+    async def help(self, interaction: discord.Interaction):
         embed = discord.Embed(
             title="Help",
             description="List of all available commands",
             color=discord.Color.blue()
         )
 
-        for command in self.bot.commands:
+        for command in self.bot.tree.walk_commands():
             embed.add_field(
                 name=f"/{command.name}",
                 value=command.description or "No description provided",
                 inline=False
             )
 
-        await ctx.send(embed=embed)
+        await interaction.response.send_message(embed=embed, ephemeral=True)
 
 async def setup(bot):
     await bot.add_cog(Help(bot))
